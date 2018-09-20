b078ef6b8f5554d83476dab473fca13f7f68f70e
atfieldson <37640441+atfieldson@users.noreply.github.com>
2018-09-19 22:16:21 -0500

    Working on Egyptian Eye

--- client.js
+++ client.js
@@ -11,6 +11,72 @@ app.controller('HandleDesignController', [function(){
     self.color1='green';
     self.color2='blue';
 
+    self.egyptianEyePattern = function (color1, color2) {
+        let bw = 400;
+        let b16 = bw / 16;
+        let pattern1 = [0, 1, 2, 3, 5, 6, 7, 8, 13, 17, 22, 24, 26, 31];
+        let pattern2 = [3, 8, 9, 10, 11, 13, 14, 15, 16];
+        let pattern3 = [0, 2, 7, 11, 16];
+        let pattern4 = [1, 6, 8, 10, 15];
+        let pattern5 = [0, 5, 9, 14, 16];
+        let row1 = [-20, -15, -10, -5, 0, 5, 10, 15, 20, 25];
+        let row2 = [-19, -14, -9, -4, 1, 6, 11, 16, 21];
+        let row3 = [-18, -13, -8, -3, 2, 7, 12, 17, 22];
+        let row4 = [-17, -12, -7, -2, 3, 8, 13, 18, 23, 28]
+        //row 5 is the else 
+        for (j = -b16 * 16; j <= b16 * 40; j += b16 * 2) {
+            for (let i = -b16 * 8; i <= bw + j; i += b16) {
+                console.log('hello');
+                c.beginPath();
+                c.moveTo(i - j, i + b16 * 2);
+                c.lineTo(i - j - b16, i + b16);
+                c.lineTo(i - j, i);
+                c.lineTo(i - j + b16, i + b16)
+                c.closePath();
+                c.stroke();
+                    //The egyptian eye does not function like Emerald did, 
+                    //when row 1 completes its first 16, it actually turns into row4, 
+                    //then row2, then row5, then row3, THEN row 1 again.  May need longer 
+                    //arrays, as I can't subract or add 16 to the 'row if's' like I could have 
+                    //done with emerald, as this pattern seems to run on a factor of 8? (16x(the five rows))
+                    //Also, I still think there is some positioning that I am not taking into account, 
+                    //the first 5 rows are correct, then it gets off.
+                    if ( row1.includes( j/ (b16*2) )){                       
+                        if (pattern1.includes(i / b16)){
+                            c.fillStyle = color1;
+                        } else{
+                            c.fillStyle = color2;
+                        }
+                    }else if ( row2.includes( j/ (b16*2) )){
+                        if (pattern2.includes(i / b16 - 2)) {
+                            c.fillStyle = color1;
+                        } else{
+                            c.fillStyle = color2;
+                        }
+                    }else if ( row3.includes( j/ (b16*2) )){                       
+                        if (pattern3.includes(i / b16 -4)) {
+                            c.fillStyle = color1;
+                        } else{
+                            c.fillStyle = color2;
+                        }
+                    }else if ( row4.includes( j/ (b16*2) )){                       
+                        if (pattern4.includes(i / b16 -6)) {
+                            c.fillStyle = color1;
+                        } else{
+                            c.fillStyle = color2;
+                        }
+                    } else{
+                        if (pattern5.includes(i / b16 -8)) {
+                            c.fillStyle = color1;
+                        } else{
+                            c.fillStyle = color2;
+                        }
+                    }                                   
+                c.fill();
+            };//end for i 
+        }//end for j
+    }//end emerald
+    
     self.emeraldPattern = function (color1, color2) {
         let bw = 400;
         let b16 = bw / 16;
