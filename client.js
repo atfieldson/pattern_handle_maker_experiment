40ec65319f809b9af902a642affae31d38bceada
atfieldson <37640441+atfieldson@users.noreply.github.com>
2018-09-23 18:07:46 -0500

    Done with Celtic, need to do accent

--- client.js
+++ client.js
@@ -11,15 +11,239 @@ app.controller('HandleDesignController', [function(){
     self.color1='green';
     self.color2='blue';
 
+    self.celticPattern = function (color1, color2) {
+        let bw = 400;
+        let b16 = bw / 16; 
+        let pattern1= [16];
+        let pattern2 = [15, 16];
+        let pattern3 =[12, 13, 16];
+        let pattern4 = [11, 12, 15, 16];
+        let pattern5 = [8, 9, 16]; 
+        let pattern6 = [7, 8, 15, 16];
+        let pattern7 = [0, 1, 4, 5, 8, 9, 10, 11, 12, 16];
+        let pattern8 = [0, 3, 4, 7, 11, 15, 16];
+        let pattern9 = [0, 1, 6, 8, 9, 10, 11,12, 13, 16];
+        let pattern10 = [0, 5, 7, 9, 12, 15, 16];
+        let pattern11 = [0, 1, 4, 5, 6, 7, 8, 11, 16];
+        let pattern12 = [0, 5, 8, 9, 10, 11, 12, 13, 14, 16];
+        let pattern13 = [0, 1, 4, 7, 9, 13, 15, 16];
+        let pattern14 = [0, 3, 4, 5, 6, 7, 8, 9, 10, 12, 16];
+        let pattern15 = [0, 1, 5, 7, 9, 11, 15, 16];
+        let pattern16 = [0, 4, 6, 7, 8, 9, 10, 11, 12, 13, 16];
+        let pattern17 = [0, 1, 3, 7, 9, 12, 15, 16];
+        let pattern18 = [0, 2, 3, 4, 5, 6, 7, 8, 11, 16];
+        let pattern19 = [0, 5, 8, 9, 10, 11, 12, 15, 16];
+        let pattern20 = [0, 1, 4, 7, 9, 11, 16];
+        let pattern21 = [0, 3, 4, 5, 6, 7, 8, 10, 15, 16];
+        let pattern22 = [0, 1, 5, 9, 12, 13, 16];
+        let pattern23 = [0, 4, 5, 6, 7, 8, 11, 12, 15, 16];
+        let pattern24 = [0, 1, 8, 9, 12, 13, 16];
+        let pattern25 = [0, 7, 8, 11, 12, 15, 16];
+        let pattern26 = [0, 1, 4, 5, 8, 9, 12, 13, 16]; 
+        let pattern27 = [0, 3, 4, 7, 8, 11, 12];
+        let pattern28 = [0, 1, 4, 5, 8, 9];
+        let pattern29 = [0, 3, 4, 7, 8]
+
+        for (j = -b16*16; j <= b16 * 40; j += b16 * 2) {
+            for (let i = -b16*8; i <= bw*2; i += b16) {
+                console.log('hello');
+                c.beginPath();
+                c.moveTo(i, i + j + 2*b16 );
+                c.lineTo(i - b16 , i + j + b16); 
+                c.lineTo(i, i + j);
+                c.lineTo(i + b16, i + j + b16)
+                c.closePath();
+                c.stroke();
+                if ( j/ (b16*2) == -8 ){                       
+                    if (pattern1.includes(i / b16)){
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -7 ){
+                    if (pattern2.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -6 ){
+                    if (pattern3.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -5 ){
+                    if (pattern4.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -4 ){
+                    if (pattern5.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -3 ){
+                    if (pattern6.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -2 ){
+                    if (pattern7.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == -1 ){
+                    if (pattern8.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 0 ){
+                    if (pattern9.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 1 ){
+                    if (pattern10.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 2 ){
+                    if (pattern11.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 3 ){
+                    if (pattern12.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 4 ){
+                    if (pattern13.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 5 ){
+                    if (pattern14.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 6 ){
+                    if (pattern15.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 7 ){
+                    if (pattern16.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 8 ){
+                    if (pattern17.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 9 ){
+                    if (pattern18.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 10 ){
+                    if (pattern19.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 11 ){
+                    if (pattern20.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 12 ){
+                    if (pattern21.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 13 ){
+                    if (pattern22.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 14 ){
+                    if (pattern23.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 15 ){
+                    if (pattern24.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 16 ){
+                    if (pattern25.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 17 ){
+                    if (pattern26.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 18 ){
+                    if (pattern27.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }else if (j/(b16*2) == 19 ){
+                    if (pattern28.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                } else {
+                    if (pattern29.includes(i / b16)) {
+                        c.fillStyle = color1;
+                    } else{
+                        c.fillStyle = color2;
+                    }
+                }
+                c.fill();
+            } //end for i
+        }//end for j
+    }//end celtic
+    
     self.egyptianEyePattern = function (color1, color2) {
         let bw = 400;
         let b16 = bw / 16;
+        
         let pattern1 = [0, 1, 2, 3, 5, 6, 7, 8, 13 ];
         let pattern2 = [3, 8, 9, 10, 11, 13, 14, 15, 16];
         let pattern3 = [0, 2, 7, 11, 16];
         let pattern4 = [1, 6, 8, 10, 15];
         let pattern5 = [0, 5, 9, 14, 16];
-        // let pattern6 = [0, 5, 6, 7, 8, 10, 11, 12, 13];
+        
         let row1 =  [-10, -5, 0, 5, 10, 15];
         let row2 = [-9, -4, 1, 6, 11, 16];
         let row3 = [-8, -3, 2, 7, 12, 17];
@@ -89,7 +313,6 @@ app.controller('HandleDesignController', [function(){
         let row1 = [-16, -12, -8, -4, 0, 4, 8, 12, 16, 20, 24, 28, 32, 36];
         let row2 = [-15, -11, -7, -3, 1, 5, 9, 13, 17];
         let row3 = [-14, -10, -6, -2, 2, 6, 10, 14, 18];
-        // let row4 = []
         for (j = -b16*16; j <= b16 * 40; j += b16 * 2) {
             for (let i = -b16*8; i <= bw*2; i += b16) {
                 console.log('hello');
